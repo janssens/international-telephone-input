4dc974aab3356a4a3e0cb2f5dbfd1ee74977f410
Jelle Janssens <janssensjelle@users.noreply.github.com>
2021-01-28 15:45:12 +0100

    Update Data.php

--- Helper/Data.php
+++ Helper/Data.php
@@ -23,7 +23,7 @@ class Data extends AbstractHelper
 
     const XML_PATH_INTERNATIONAL_TELEPHONE_MULTISELECT_COUNTRIES_ALLOWED = 'internationaltelephoneinput/general/allow';
 
-    const XML_PATH_PREFERED_COUNTRY = 'general/store_information/country_id';  
+    const XML_PATH_PREFERED_COUNTRY = 'internationaltelephoneinput/general/preferedCountry';
     
     /**
      * @var StoreManagerInterface
@@ -104,7 +104,8 @@ class Data extends AbstractHelper
             'validation' => [
                 "required-entry"    => true,
                 "max_text_length"   => 255,
-                "min_text_length"   => 1
+                "min_text_length"   => 1,
+                'validate-phone-number' => true
             ],
             'options' => [],
             'filterBy' => null,
